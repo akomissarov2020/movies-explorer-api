00a69d555f95eb08b9ce3bfa011e313afc372f66
Aleksey Komissarov <ad3002@gmail.com>
2022-03-03 19:08:33 +0300

    feat/file structure generation

---
+++ backend_structure.py
@@ -0,0 +1,38 @@
+
+
+import os
+
+if __name__ == '__main__':
+    
+    folders = {
+        "utils": "utils",
+        "routes": "routes",
+        "models": "models",
+        "middlewares": "middlewares",
+        "logs": "logs",
+        "errors": "errors",
+        "controllers": "controllers",
+    }
+
+    files = {
+        "app.js": "app.js",
+    }
+
+    models = ["user", "movie"]
+
+    for folder in folders.values():
+        if not os.path.isdir(folder):
+            os.makedirs(folder)
+
+    for file_name in files.values():
+        if not os.path.isfile(file_name):
+            with open(file_name, "w") as fh:
+                pass
+
+    for model in models:
+        for folder in ["routes", "models", "controllers"]:
+            file_name = os.path.join(folder, model + ".js")
+            if not os.path.isfile(file_name):
+                with open(file_name, "w") as fh:
+                    pass
+
