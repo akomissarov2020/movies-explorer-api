752428f8f94f22e5b365ded9f6ef2741184fd927
akomissarov2020 <79610245+akomissarov2020@users.noreply.github.com>
2022-07-03 10:04:59 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,7 +14,7 @@ module.exports.handleCORsOptionsRequest = (req, res, next) => {
       res.header("Access-Control-Allow-Headers", "authorization,Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers");
       return res.end();
     }
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', origin);
   }
   return next();
 };
