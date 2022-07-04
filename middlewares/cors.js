752c6f2a49a47cc3210177f6c1167b8d2d7d0f13
akomissarov2020 <79610245+akomissarov2020@users.noreply.github.com>
2022-07-05 02:54:45 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,7 +6,8 @@ module.exports.handleCORsOptionsRequest = (req, res, next) => {
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
 
-  if (allowedCORS.includes(origin)) {
+  #if (allowedCORS.includes(origin)) {
+   if (true) {
     if (method === 'OPTIONS') {
       res.header("Access-Control-Allow-Origin", origin);
       res.header('Access-Control-Allow-Credentials', "true");
@@ -17,8 +18,5 @@ module.exports.handleCORsOptionsRequest = (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', "true");
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  res.header("Access-Control-Allow-Headers", "authorization,Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers");
-  res.header('Access-Control-Allow-Origin', origin);
-  return res.end();
+  return next();
 };
