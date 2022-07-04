b58ec65423e42d3388a6c49b4b521983d3425ebd
akomissarov2020 <79610245+akomissarov2020@users.noreply.github.com>
2022-07-05 02:22:13 +0300

    Update cors_settings.js

--- cors_settings.js
+++ cors_settings.js
@@ -4,7 +4,7 @@ const allowedCORS = [
   'https://smotrelka.nomoredomains.work/',
   'http://api.smotrelka.nomoredomains.work/',
   'https://api.smotrelka.nomoredomains.work/',
-  'http://smotrelka.nomoredomains.work/,
+  'http://smotrelka.nomoredomains.work/',
   'https://smotrelka.nomoredomains.work/',
   'localhost:3000',
   'http://localhost:3000',
